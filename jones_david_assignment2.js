b5b37b1afed1fc49883bb15d8397de6a61816b4e
David Jones <dljones79@fullsail.edu>
2013-03-14 17:46:22 -0500

    Added some variables and first function.

--- jones_david_assignment2.js
+++ jones_david_assignment2.js
@@ -1,1 +1,27 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// David Jones
+// SDI 1303
+// Project 2
+
+// Variables
+
+var target = 1
+	campSpot = "right",
+	warded = true,
+	mobPulled = true,
+	mezAdds = false,
+	mobDead = true
+; // Variables
+
+// Procedural Function for determining if target is spotted.
+var targetSpotted = function(spotted){
+	if (spotted === true) {
+		console.log ("I have spotted the target!");
+	} else {
+		console.log ("Let's wait for a respawn.");
+	}
+}; //targetSpotted
+
+targetSpotted(true);
+
